64826407ebda347510cad863fac3953bd7652c19
MartijnDeVries <martijn.de.vries@windesheim.nl>
2025-04-05 23:03:09 +0200

    init vintago package
    CRUD functionality for artikelen on aan MVC based structure

---
+++ src/main/java/org/vfl/vintago/VintaGoApplication.java
@@ -0,0 +1,13 @@
+package org.vfl.vintago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintaGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vfl/vintago/VintaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vfl.vintago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
