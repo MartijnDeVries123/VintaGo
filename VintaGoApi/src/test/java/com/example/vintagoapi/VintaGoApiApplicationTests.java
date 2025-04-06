3faf31ff74075457e8c43fd62fc672dd0694f0ba
Rick van Stralen <rickvanstralen@hotmail.com>
2025-04-06 12:51:52 +0200

    base for the api and added docker

---
+++ VintaGoApi/src/test/java/com/example/vintagoapi/VintaGoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vintagoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintaGoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
