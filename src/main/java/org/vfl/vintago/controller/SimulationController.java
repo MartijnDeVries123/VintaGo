75e830a46e16649f822bc24276abfe1229a88f22
MartijnDeVries <martijn.de.vries@windesheim.nl>
2025-05-05 22:34:10 +0200

    Controlling simulation initialization with input from frontend api

---
+++ src/main/java/org/vfl/vintago/controller/SimulationController.java
@@ -0,0 +1,22 @@
+package org.vfl.vintago.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.vfl.vintago.dto.SimulationTypeDTO;
+import org.vfl.vintago.service.SimulationTypeDispatcher;
+
+@RestController
+@RequestMapping("/simulationtypes")
+public class SimulationController {
+
+    @Autowired
+    SimulationTypeDispatcher dispatcher;
+
+
+    @PostMapping
+    public ResponseEntity<Void> runSimulationType(@RequestBody SimulationTypeDTO request) {
+        dispatcher.dispatch(request.getType());
+        return ResponseEntity.ok().build();
+    }
+}
