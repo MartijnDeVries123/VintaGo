45ac22071728313e235ed8e20deb2099b8846e17
MartijnDeVries <martijn.de.vries@windesheim.nl>
2025-05-07 19:34:42 +0200

    controlling retrieving route data with service to hande logic

---
+++ src/main/java/org/vfl/vintago/controller/RouteScheduleController.java
@@ -0,0 +1,24 @@
+package org.vfl.vintago.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.vfl.vintago.dto.RouteDTO;
+import org.vfl.vintago.service.RouteScheduleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/routes-schedule")
+public class RouteScheduleController {
+    @Autowired
+    RouteScheduleService routeScheduleService;
+
+    @GetMapping
+    public ResponseEntity<List<RouteDTO>> getCompleteSchedule() {
+        List<RouteDTO> schedule = routeScheduleService.getCompleteSchedule();
+        return ResponseEntity.ok(schedule);
+    }
+}
