9477fdfcff3e23961caa12bcb6b3bc679024b1e7
MartijnDeVries <martijn.de.vries@windesheim.nl>
2025-05-07 14:11:03 +0200

    Re-added addressDTO

---
+++ src/main/java/org/vfl/vintago/dto/AddressDTO.java
@@ -0,0 +1,38 @@
+package org.vfl.vintago.dto;
+
+public class AddressDTO {
+    private final Long id;
+    private final String street;
+    private final String number;
+    private final String city;
+    private final String zip;
+    private final CoordinateDTO location;
+
+    public AddressDTO(Long id, String street, String number, String city, String zip, CoordinateDTO location) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.zip = zip;
+        this.location = location;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public String getNumber() {
+        return number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public CoordinateDTO getLocation() {
+        return location;
+    }
+}
