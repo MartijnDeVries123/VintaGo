2c4c9bb0c247251cd14df636c0bc5017e6002cbb
MartijnDeVries <martijn.de.vries@windesheim.nl>
2025-05-10 17:04:37 +0200

    Refactor `VrpSolver` to an abstract class and add utility methods.

--- src/main/java/org/vfl/algorithms/VrpSolver.java
+++ src/main/java/org/vfl/algorithms/VrpSolver.java
@@ -1,14 +1,44 @@
 package org.vfl.algorithms;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.vfl.vintago.entity.Address;
+import org.vfl.vintago.entity.*;
+import org.vfl.vintago.repository.RouteRepository;
+import org.vfl.vintago.util.DistanceMatrix;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
-public interface VrpSolver {
-    List<Integer> solve(List<Address> unfulfilledOrders, String days);
-    boolean canCreateMultipleRoutes();
+public abstract class VrpSolver {
+    @Autowired RouteRepository routeRepository;
+
+    protected static final Address WINDESHEIM_DEPOT;
+    static {
+        Address depot = new Address();
+        depot.setStreet("Campus 2");
+        depot.setZip("8017CA");
+        depot.setCity("Zwolle");
+        depot.setLat(52.4964);
+        depot.setLng(6.0846);
+        WINDESHEIM_DEPOT = depot;
+    }
+
+    protected long[][] getDistanceMatrix(List<Address> addresses) {
+        return DistanceMatrix.getDistanceMatrix(addresses);
+    }
+    protected Map<Address, Integer> mapAddressToIndex(List<Address> addresses) {
+        Map<Address, Integer> addressToIndex = new HashMap<>();
+        for (int i = 0; i < addresses.size(); i++) {
+            addressToIndex.put(addresses.get(i), i);
+        }
+        return addressToIndex;
+    }
+
+    public abstract List<Route> solve(List<Address> unfulfilledOrders, int days);
 
     // Todo. Dit moet verplaatst worden naar een eigen service. Een LoggingService zou bij uitstek geschikt zijn.
 //    default List<Integer> findShortestRouteTimingMethod(double[][] distanceMatrix) {
@@ -27,4 +57,48 @@ public interface VrpSolver {
 //        System.out.println("\nExecution time: " + (elapsedTimeNs) + "ns = " + String.format("%.1f", elapsedTimeMs) + "ms");
 //        return shortestRoute;
 //    }
+    protected Route saveRoute(
+            LocalDate deliveryDate,
+            DeliveryTruck deliveryTruck,
+            List<Address> solvedAddresses
+    ) {
+        Route route = new Route();
+        route.setDeliveryDate(deliveryDate);
+        route.setDeliveryTruck(deliveryTruck);
+
+        List<RouteAddress> routeAddresses = new ArrayList<>();
+        for (int i = 0; i < solvedAddresses.size(); i++) {
+            if (solvedAddresses.get(i).equals(WINDESHEIM_DEPOT)) continue;
+
+            Address address = solvedAddresses.get(i);
+            address.setStatus("pending");
+
+            RouteAddress routeAddress = new RouteAddress();
+            routeAddress.setRoute(route);
+            routeAddress.setAddress(address);
+            routeAddress.setStepOrder(i + 1);
+
+            RouteAddressId id = new RouteAddressId();
+            id.setRouteId(null); // Wordt na persist ingevuld
+            id.setAddressId(address.getId());
+            routeAddress.setId(id);
+
+            routeAddresses.add(routeAddress);
+        }
+
+        route.setRouteAddresses(routeAddresses);
+
+        routeRepository.save(route);
+        return route;
+    }
+
+    protected double calculateTotalDistance(List<Address> route, Map<Address, Integer> addressToIndex, long[][] distanceMatrix) {
+        double total = 0;
+        for (int i = 0; i < route.size() - 1; i++) {
+            int fromIndex = addressToIndex.get(route.get(i));
+            int toIndex = addressToIndex.get(route.get(i + 1));
+            total += distanceMatrix[fromIndex][toIndex];
+        }
+        return total;
+    }
 }
