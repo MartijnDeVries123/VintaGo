fba72503550c72bfc0ca252868c0e936488bfd64
MartijnDeVries <martijn.de.vries@windesheim.nl>
2025-05-11 12:24:49 +0200

    Refactor Lin-Kernighan algorithm for VRP integration
    Integrated the Lin-Kernighan algorithm into a VRP workflow with dependency injection, clustering, and routing capabilities. Replaced legacy distance calculation methods with structured distance matrix usage and improved TSP handling. Added support for saving and scheduling delivery routes with dependency on repository and service components.

--- src/main/java/org/vfl/vintago/service/SolverResolver.java
+++ src/main/java/org/vfl/vintago/service/SolverResolver.java
@@ -3,6 +3,7 @@ package org.vfl.vintago.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.vfl.algorithms.BruteForce;
+import org.vfl.algorithms.LinKernighanAlgorithm;
 import org.vfl.algorithms.OrTools;
 import org.vfl.algorithms.VrpSolver;
 
@@ -10,11 +11,13 @@ import org.vfl.algorithms.VrpSolver;
 public class SolverResolver {
     @Autowired BruteForce bruteForce;
     @Autowired OrTools orTools;
+    @Autowired LinKernighanAlgorithm linKernighanAlgorithm;
 
     public VrpSolver getSolver(String solver) {
       return  switch (solver) {
             case "bf" ->  bruteForce;
             case "ortools" -> orTools;
+            case "lk" -> linKernighanAlgorithm;
             default -> null;
         };
 
