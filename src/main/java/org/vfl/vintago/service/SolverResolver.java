4fce0e782e01f4320b8c3cacd0b519e192d2ee6a
MartijnDeVries <martijn.de.vries@windesheim.nl>
2025-05-11 15:52:03 +0200

    Add GeneticAlgorithm to SolverResolver and refine implementation

--- src/main/java/org/vfl/vintago/service/SolverResolver.java
+++ src/main/java/org/vfl/vintago/service/SolverResolver.java
@@ -2,22 +2,21 @@ package org.vfl.vintago.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.vfl.algorithms.BruteForce;
-import org.vfl.algorithms.LinKernighanAlgorithm;
-import org.vfl.algorithms.OrTools;
-import org.vfl.algorithms.VrpSolver;
+import org.vfl.algorithms.*;
 
 @Service
 public class SolverResolver {
     @Autowired BruteForce bruteForce;
     @Autowired OrTools orTools;
     @Autowired LinKernighanAlgorithm linKernighanAlgorithm;
+    @Autowired GeneticAlgorithm geneticAlgorithm;
 
     public VrpSolver getSolver(String solver) {
       return  switch (solver) {
             case "bf" ->  bruteForce;
             case "ortools" -> orTools;
             case "lk" -> linKernighanAlgorithm;
+            case "ga" -> geneticAlgorithm;
             default -> null;
         };
 
