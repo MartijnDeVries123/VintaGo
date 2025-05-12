57138c7bcff5c5b31a53bbf7c983929e1d9114a5
Rick van Stralen <rickvanstralen@hotmail.com>
2025-05-12 22:36:23 +0200

    Added BruteForceTest and did some cleaning

--- src/test/java/org/vfl/vintago/util/RouteDistanceCalculatorTest.java
+++ src/test/java/org/vfl/vintago/util/RouteDistanceCalculatorTest.java
@@ -17,16 +17,16 @@ class RouteDistanceCalculatorTest {
         Route route = new Route();
         List<Address> addresses = new ArrayList<>();
 
-        addresses.add(createAddress(1L, 52.379189, 4.899431));
-        addresses.add(createAddress(2L, 51.9225, 4.47917));
-        addresses.add(createAddress(3L, 52.0705, 4.3007));
-        addresses.add(createAddress(4L, 52.0907, 5.1214));
-        addresses.add(createAddress(5L, 51.4416, 5.4697));
-        addresses.add(createAddress(6L, 53.2194, 6.5665));
-        addresses.add(createAddress(7L, 52.5200, 5.7480));
-        addresses.add(createAddress(8L, 51.5866, 4.7750));
-        addresses.add(createAddress(9L, 52.5050, 6.0900));
-        addresses.add(createAddress(10L, 51.8126, 5.8372));
+        addresses.add(createAddress(52.379189, 4.899431));
+        addresses.add(createAddress(51.9225, 4.47917));
+        addresses.add(createAddress(52.0705, 4.3007));
+        addresses.add(createAddress(52.0907, 5.1214));
+        addresses.add(createAddress(51.4416, 5.4697));
+        addresses.add(createAddress(53.2194, 6.5665));
+        addresses.add(createAddress(52.5200, 5.7480));
+        addresses.add(createAddress(51.5866, 4.7750));
+        addresses.add(createAddress(52.5050, 6.0900));
+        addresses.add(createAddress(51.8126, 5.8372));
 
         List<RouteAddress> routeAddresses = new ArrayList<>();
         int step = 1;
@@ -47,7 +47,7 @@ class RouteDistanceCalculatorTest {
         assertEquals(expectedTotal, total, "De berekende afstand moet exact overeenkomen met de verwachte waarde");
     }
 
-    private Address createAddress(Long id, double lat, double lng) {
+    private Address createAddress(double lat, double lng) {
         Address address = new Address();
         address.setLat(lat);
         address.setLng(lng);
